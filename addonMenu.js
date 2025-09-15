30c2caf2ba629d683d59e85ccca99fbc9bdceba3
meatbroc <168386225+meatbroc@users.noreply.github.com>
2025-09-15 12:37:45 -0400

    Update addonMenu.js
    created a keyboard shortcut adding method
    @todo - make sure everything works

--- addonMenu.js
+++ addonMenu.js
@@ -14,10 +14,10 @@
     class AddonMenu {
         static #instance;
         static #pref = (geofs.preferences.aMenu ||= {});
-        static debugging = !1;
         constructor() {
             if (AddonMenu.#instance) return AddonMenu.#instance;
             AddonMenu.#instance = this;
+            this.debugging = !1;
             window.executeOnEventDone("geofsInitialized", this.init.bind(this)); // .bind forces init to use an AddonMenu instance as the "this" keyword instead of whatever is calling .init
         }
         button;
@@ -47,10 +47,51 @@
             window.fireBasicEvent("addonMenuInitialized");
         }
         static addPreference(name, defaultVal, type) {
-            if (geofs.preferences.aMenu[name]) return AddonMenu.debugging && console.error(`geofs.preferences.aMenu.${name} already added`), !0;
+            if (geofs.preferences.aMenu[name]) return this.debugging && console.error(`geofs.preferences.aMenu.${name} already added`), !1;
             if (geofs.preferencesDefault.aMenu[name]) return console.error(`default preference ${name} conflict`), !1;
             if (type === "checkbox" && typeof defaultVal !== "boolean") return console.error("checkbox default values can only be true or false"), !1;
             return AddonMenu.#pref[name] = geofs.preferencesDefault.aMenu[name] = defaultVal, true;
+        }
+        static populateKeyAssignments = function(e) {
+            var t = "";
+            for (var a in geofs.preferences.aMenu.keys) {
+                var o = "keyInput" + geofs.preferences.aMenu.keys[a].keycode;
+                t += '<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input id="' + o + '" class="geofs-preferences-key-detect mdl-textfield__input" type="text" data-type="keydetect" data-gespref="geofs.preferences.aMenu.keys.' + a + '" keycode="' + geofs.preferences.aMenu.keys[a].keycode + '" value="' + geofs.preferences.aMenu.keys[a].label + '"/><label class="mdl-textfield__label" for="' + o + '">' + a + "</label></div>"
+            }
+            e.html(t),
+            e.on("click focus", ".geofs-preferences-key-detect", t => {
+                $(".geofs-preference-key-detecting", e).each( (e, t) => {
+                    t.value = t._originalValue,
+                    $(t).removeClass("geofs-preference-key-detecting")
+                }
+                ),
+                t.currentTarget._originalValue = t.currentTarget.value,
+                t.currentTarget.value = "",
+                $(t.currentTarget).addClass("geofs-preference-key-detecting")
+            }
+            ).on("keyup", ".geofs-preferences-key-detect", e => {
+                var t, a = e.currentTarget;
+                $(a).hasClass("geofs-preference-key-detecting") && (27 != e.which ? (t = geofs.preferencesKeycodeLookup[e.which] ? geofs.preferencesKeycodeLookup[e.which] : a.value.toUpperCase(),
+                a.value = t,
+                a.setAttribute("keycode", e.which),
+                geofs.setPreferenceFromInput(a)) : a.value = a._originalValue,
+                $(a).removeClass("geofs-preference-key-detecting"),
+                $(a).blur(),
+                e.stopPropagation(),
+                e.preventDefault())
+            }
+            ).on("keydown", ".geofs-preferences-key-detect", e => {
+                $(e.currentTarget).hasClass("geofs-preference-key-detecting") && 9 == e.which && (e.stopPropagation(),
+                e.preventDefault())
+            }
+            ).on("blur", ".geofs-preferences-key-detect", e => {
+                $(e.currentTarget).hasClass("geofs-preference-key-detecting") && ("" == e.currentTarget.value && (e.currentTarget.value = e.currentTarget._originalValue),
+                $(e.currentTarget).removeClass("geofs-preference-key-detecting"),
+                e.stopPropagation(),
+                e.preventDefault())
+            }
+            ),
+            componentHandler.upgradeDom()
         }
         setPreferenceValues = function() {
             $(this.$menu).find("[data-gespref]").each( (e, a) => {
@@ -82,7 +123,7 @@
                 o.prop("checked", d),
                 d ? o.parent(".mdl-radio, .mdl-switch").addClass("is-checked") : o.parent(".mdl-radio, .mdl-switch").removeClass("is-checked");
             },
-            "keydetect": null // geofs wants to skip the default for this kind of input, even though the game has none of these
+            "keydetect": null // geofs wants to skip the default for this kind of input because the key detection inputs use this [data-type] attribute
         };
         /**
          * @description - adds a supported input type to be read at startup
@@ -90,8 +131,8 @@
          * @param {Function} t - callback function to be executed on init to initialise preference setting
         **/
         addSupportedInputType(e, t) {
-            if (!e || !t) return AddonMenu.debugging && console.error("cannot support input type because of invalid type or callback"), !1;
-            AddonMenu.#supportedInputTypes[e] = t;
+            if (!e || !t) return this.debugging && console.error("cannot support input type because of invalid type or callback"), !1;
+            return AddonMenu.#supportedInputTypes[e] === null ? (this.debugging && console.error('input type is intentionally ignored'), !1) : (AddonMenu.#supportedInputTypes[e] = t, !0);
         }
     }
     class AddonMenuItem {
@@ -116,6 +157,14 @@
             inp.attr(options); // shorthands huge conditional for checkboxes
             this.$element.append(`<span style="text-indent: ${level}rem">${description}</span>`, inp, '<br>');
             return this;
+        }
+        /**
+         * @description - adds a keyboard shortcut input to the addon menu.
+         * @param {object} defaultValue - object styled after a geofs.preferencesDefault.keyboard.keys entry, so something like this. { "Toggle Autopilot": { keycode: 65, label: "<A>" }
+        **/
+        addKeyboardShortcut(prefId, defaultValue, callback) {
+            AddonMenu.addPreference(prefId, defaultValue, "keydetect");
+            AddonMenu.populateKeyAssignments(this.$element);
         }
         addToMenu(panel = "#addonMenu") {
             return $(panel).append(this.$element), this;
